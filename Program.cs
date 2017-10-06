9505351ee92d87ef9714c1377059571c231c4643
FIC <alandermm@hotmail.com>
2017-10-06 19:31:50 -0300

    modificações

--- Program.cs
+++ Program.cs
@@ -9,17 +9,37 @@ namespace CadastroPessoa
            Console.WriteLine("Qual é o seu nome?");
            string nome = Console.ReadLine(); 
 
-           Console.WriteLine("Qual é o seu RG?");
+           Console.WriteLine("RG:");
            string rg = Console.ReadLine(); 
 
-           Console.WriteLine("Qual é o seu CPF?");
+           Console.WriteLine("CPF:");
            string cpf = Console.ReadLine(); 
 
-           Console.WriteLine("Qual é a sua idade?");
-           int idade = Console.ReadLine();
+           Console.WriteLine("Idade:");
+           string idade = Console.ReadLine();
 
-           Console.WriteLine("Qual é o seu gênero?");
-           string genero = Console.ReadLine(); 
+           Console.WriteLine("Gênero:");
+           string genero = Console.ReadLine();
+
+           Console.WriteLine("Estado civil:");
+           string estado_civil = Console.ReadLine();
+
+           Console.WriteLine("Profissão:");
+           string profissao = Console.ReadLine();
+
+           Console.WriteLine("Local de Nascimento:");
+           string local_nascimento = Console.ReadLine();
+
+           Console.WriteLine("Endereço:");
+           string endereco = Console.ReadLine();
+
+           Console.WriteLine("Cidade:");
+           string cidade = Console.ReadLine();
+
+           Console.WriteLine("Nome: " + nome + ", RG: " + rg + ", CPF: " + cpf);
+           Console.WriteLine("Idade: " + idade + ", Gênero: " + genero + ", Estado Civil: " + estado_civil);
+           Console.WriteLine("Profissao: " + profissao + ", Local de Nascimento: " + local_nascimento );
+           Console.WriteLine("Endereço: " + endereco + ", Cidade: " + cidade);
         }
     }
 }
