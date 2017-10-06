2bd15060b8042b359e6351b54826a4ecd690264d
FIC <paulasammei@gmail.com>
2017-10-06 19:07:02 -0300

    Atualização Paula

--- Program.cs
+++ Program.cs
@@ -9,10 +9,17 @@ namespace CadastroPessoa
            Console.WriteLine("Qual é o seu nome?");
            string nome = Console.ReadLine(); 
 
+           Console.WriteLine("Qual é o seu RG?");
+           string rg = Console.ReadLine(); 
+
+           Console.WriteLine("Qual é o seu CPF?");
+           string cpf = Console.ReadLine(); 
+
            Console.WriteLine("Qual é a sua idade?");
            int idade = Console.ReadLine();
 
-           
+           Console.WriteLine("Qual é o seu gênero?");
+           string genero = Console.ReadLine(); 
         }
     }
 }
